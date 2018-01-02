7ca1b415f5c80501a395dd85e7fe24cf6f2e2d4a
Jalal <mohd.farhan.jalal@intel.com>
2018-01-02 10:48:10 +0800

    oauth init

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+    google:{
+        clientID:'882742290115-c7dug47goathntdlp5tvdn8m3lksb6h5.apps.googleusercontent.com',
+        clientSecret:'vJZj4oGwGoxQ0f2d41KW3fYb'
+    }
+};
