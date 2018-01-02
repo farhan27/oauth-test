7a5586006ab3313c04c8e4c4db09efbb48745eb2
Jalal <mohd.farhan.jalal@intel.com>
2018-01-02 15:52:47 +0800

    add mongoose

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,8 @@ module.exports = {
     google:{
         clientID:'882742290115-c7dug47goathntdlp5tvdn8m3lksb6h5.apps.googleusercontent.com',
         clientSecret:'vJZj4oGwGoxQ0f2d41KW3fYb'
+    },
+    mongodb:{
+        dbURI:'mongodb://mjalal:1234@ds139067.mlab.com:39067/mjalal-oauth-test'
     }
 };
